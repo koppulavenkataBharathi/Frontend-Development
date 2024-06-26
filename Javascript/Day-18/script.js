84486b15a26cdaf8f08e0a22b0a1bf1db02bb2cb
koppulavenkataBharathi <koppulabharathi329@gmail.com>
2024-06-26 18:29:19 +0530

    day 18 task submited

---
+++ Javascript/Day-18/script.js
@@ -0,0 +1,29 @@
+var Submit = document.getElementById("submit-button");
+
+Submit.addEventListener('click', function() {
+    var Name = document.getElementById("name");
+    var Mobile = document.getElementById('mobile');
+    var Gender = document.getElementById('gender');
+    var Dob = document.getElementById('dob');
+    var Address=document.getElementById('address');
+    var Output = document.getElementsByClassName("display-output")[0]; 
+    
+
+    // Create an array of the input values
+    var infoArray = [
+        `Name: ${Name.value}`,
+        `Mobile: ${Mobile.value}`,
+        `Gender: ${Gender.value}`,
+        `DOB: ${Dob.value}`,
+        `Address:${Address.value}`
+    ];
+    
+    // Loop through the array and create a new element for each piece of information
+    for (var i = 0; i < infoArray.length; i++) {
+        var newElement = document.createElement('h4');
+        newElement.textContent = infoArray[i];
+        Output.appendChild(newElement); // Append the new element to the output
+    }
+
+    console.log(Output);
+});
